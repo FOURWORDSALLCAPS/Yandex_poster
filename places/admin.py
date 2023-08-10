8acc31b4e92f3fb037d525d2505f45c7e3d542ea
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-10 15:51:26 +0300

    Add preview picture in admin

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.html import format_html
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        max_height = 200
+        return format_html(
+            '<img src="{}" style="max-height:{}px" />',
+            obj.imgs.url,
+            max_height
+        )
 
 
 @admin.register(Place)
