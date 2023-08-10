ffae0fdf8fecc505f7b6b7163c0c2db9e40294ba
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-10 10:25:55 +0300

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+
+class Place(models.Model):
+    title = models.CharField('Название', max_length=200)
+    imgs = models.ImageField('Картинка', upload_to='places', blank=True)
+    description_short = models.TextField('Краткое описание')
+    description_long = models.TextField('Длинное описание')
+    lng = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
+
+    def __str__(self):
+        return self.title
