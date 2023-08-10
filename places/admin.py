1727347910a4663e17e80bfda60d64d5e7f03c16
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-10 15:43:10 +0300

    Add inlines in admin

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ImageInline]
 
 
 @admin.register(Image)
