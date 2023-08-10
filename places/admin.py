6d89449f45711c3fd5a7d857c39c68d142c98c1c
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-10 16:15:19 +0300

    Numbering hiddenHide numbering in admin

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Place, Image
 from django.utils.html import format_html
+from adminsortable2.admin import SortableTabularInline, SortableAdminBase
 
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableTabularInline):
     model = Image
     readonly_fields = ['get_preview']
 
@@ -17,7 +18,7 @@ class ImageInline(admin.TabularInline):
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [ImageInline]
 
 
